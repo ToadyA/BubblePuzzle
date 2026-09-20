7265f2a0a7d43981fe8b0bd1393395a4379d8749
Asher Roberts <asher.roberts98@gmail.com>
2026-09-20 02:50:11 -0700

    I am awake and coding again. FIght me.

--- bubblepuzzle.js
+++ bubblepuzzle.js
@@ -1,8 +1,38 @@
 
+//10x6 grid of sediment and air.
+let sedimentBoogey = 10;
+for(let i = 0; i < 6; i ++){
+    for(let j = 0; j < 10; j ++){
+        //if(){
+            let sediment = document.createElement("img");
+            sediment.src = "images/bubbles/sediment.png";
+            sediment.className = "gridworks";
+            sedimentBoogey = 10 + (j * 8);
+            sediment.style.left = sedimentBoogey + "%";
+            sedimentBoogey = 17 + (i * 12);
+            sediment.style.top = sedimentBoogey + "%";
+            document.body.appendChild(sediment);
+            /*
+        }
+        else{
+            let airhead = document.createElement("img");
+            airhead.src = "images/twinkle0.png";
+            airhead.className = "gridworks";
+            sedimentBoogey = 10 + (j * 8);
+            airhead.style.left = sedimentBoogey + "%";
+            sedimentBoogey = 17 + (i * 12);
+            airhead.style.top = sedimentBoogey + "%";
+            document.body.appendChild(airhead);
+        }
+            */
+    }
+}
+
 hotly = 0;
 hotspots = ["10%", "18%", "26%", "34%", "42%", "50%", "58%", "66%", "74%", "82%"];
 player = document.getElementById("player");
 gauge = document.getElementById("gauge");
+meter = document.getElementById("meter");
 munkTracks = 10;
 munkMind = 15;
 document.addEventListener('keydown', (e) =>{
@@ -22,7 +52,8 @@ document.addEventListener('keydown', (e) =>{
     }
     if(e.key === 'Space') {
         player.src = "images/chipBlow.png";
-        gauge.style.left = (2 + parseInt(player.style.left)) + "%";
+        //perform every check known to man: four for each slot because there is no earthly way to know where the current position is from the perspective of a given slot.
+        //do I need 240 checks every time?
     }
     if(e.key === '0'){
         cursor.style.left = hotspots[0];
@@ -83,6 +114,9 @@ function leftBehind(){
             munkMind = munkMind - 0.5;
             player.style.left = munkTracks + "%";
             gauge.style.left = munkMind + "%";
+            munkMind = munkMind + 0.2;
+            meter.style.left = munkMind + "%";
+            munkMind = munkMind - 0.2;
             player.src = "images/chipwalk2.png";
             console.log("Cursor left is greater than chip left; chip x: " + player.style.left + " cursor x: " + cursor.style.left);
             leftBehind();
@@ -92,6 +126,9 @@ function leftBehind(){
             munkMind = munkMind + 0.5;
             player.style.left = munkTracks + "%";
             gauge.style.left = munkMind + "%";
+            munkMind = munkMind + 0.2;
+            meter.style.left = munkMind + "%";
+            munkMind = munkMind - 0.2;
             player.src = "images/chipwalk0.png";
             console.log("Cursor left is less than chip left; chip x: " + player.style.left + " cursor x: " + cursor.style.left);
             leftBehind();
